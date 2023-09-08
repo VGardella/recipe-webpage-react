82f069a0e96c681228aa34d984625b4386e214c5
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-09-08 13:06:36 -0300

    feat: footer

---
+++ src/components/layout/footer.jsx
@@ -0,0 +1,20 @@
+export function Footer() {
+    return(
+        <div id='footer'>
+            <div id='signature'>
+                <p>Made by Victoria Gardella</p>
+            </div>
+            <div id='media-links'>
+                <a href='' alt=''>
+                    <img src='' alt=''></img>
+                </a>
+                <a href='' alt=''>
+                    <img src='' alt=''></img>
+                </a>
+                <a href='' alt=''>
+                    <img src='' alt=''></img>
+                </a>
+            </div>
+        </div>
+    )
+}
