6deada90d7b67c8d018f934207f103c465eedd90
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-09-09 09:32:44 -0300

    add: media icons and classes

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -1,18 +1,27 @@
 export function Footer() {
     return(
-        <div id='footer'>
+        <div className='footer'>
             <div id='signature'>
                 <p>Made by Victoria Gardella</p>
             </div>
             <div id='media-links'>
-                <a href='' alt=''>
-                    <img src='' alt=''></img>
+                <a className='external' href='' alt=''>
+                    <img className='media-icon' id='github' src='../src/assets/github.png' alt='Link to GitHub Profile'
+                        onMouseOver={e => (e.currentTarget.src='../src/assets/github-hover.png')}
+                        onMouseOut={e => (e.currentTarget.src='../src/assets/github.png')}
+                    ></img>
                 </a>
-                <a href='' alt=''>
-                    <img src='' alt=''></img>
+                <a className='external' href='' alt=''>
+                    <img className='media-icon' id='linkedin' src='../src/assets/linkedin.png' alt=''
+                        onMouseOver={e => (e.currentTarget.src='../src/assets/linkedin-hover.png')}
+                        onMouseOut={e => (e.currentTarget.src='../src/assets/linkedin.png')}
+                    ></img>
                 </a>
-                <a href='' alt=''>
-                    <img src='' alt=''></img>
+                <a className='external' href='' alt=''>
+                    <img className='media-icon' id='mail' src='../src/assets/mail.png' alt=''
+                        onMouseOver={e => (e.currentTarget.src='../src/assets/mail-hover.png')}
+                        onMouseOut={e => (e.currentTarget.src='../src/assets/mail.png')}
+                    ></img>
                 </a>
             </div>
         </div>
