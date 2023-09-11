cc2cc89e54c8a476accc06990a0a42e62584cb88
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-09-11 17:25:30 -0300

    add: mock info for recipes

---
+++ src/assets/recipes.js
@@ -0,0 +1,73 @@
+let cookies = {
+    id: 0,
+    img: './src/assets/cookies.jpg',
+    title: 'Cookies',
+    category: 'Dessert',
+    description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
+}
+
+let twoCookies = [{
+    id: 0,
+    img: './src/assets/cookies.jpg',
+    title: 'Cookies',
+    category: 'Dessert',
+    description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
+}, {
+    id: 1,
+    img: './src/assets/cookies.jpg',
+    title: 'Cookies',
+    category: 'Dessert',
+    description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
+}]
+
+let lotsOfCookies = [{
+    id: 0,
+    img: './src/assets/cookies.jpg',
+    title: 'Cookies',
+    category: 'Dessert',
+    description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
+}, {
+    id: 1,
+    img: './src/assets/cookies.jpg',
+    title: 'Cookies',
+    category: 'Dessert',
+    description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
+}, {
+    id: 2,
+    img: './src/assets/cookies.jpg',
+    title: 'Cookies',
+    category: 'Dessert',
+    description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
+}, {
+    id: 3,
+    img: './src/assets/cookies.jpg',
+    title: 'Cookies',
+    category: 'Dessert',
+    description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
+}, {
+    id: 4,
+    img: './src/assets/cookies.jpg',
+    title: 'Cookies',
+    category: 'Dessert',
+    description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
+}, {
+    id: 5,
+    img: './src/assets/cookies.jpg',
+    title: 'Cookies',
+    category: 'Dessert',
+    description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
+}, {
+    id: 6,
+    img: './src/assets/cookies.jpg',
+    title: 'Cookies',
+    category: 'Dessert',
+    description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
+}, {
+    id: 7,
+    img: './src/assets/cookies.jpg',
+    title: 'Cookies',
+    category: 'Dessert',
+    description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
+}]
+
+export { cookies, twoCookies, lotsOfCookies }
