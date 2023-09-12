a30cfef8e64986c8407c2c19959e0c123cfed5b5
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-09-12 18:17:20 -0300

    add: mock info (short description)

--- src/assets/recipes.js
+++ src/assets/recipes.js
@@ -3,6 +3,7 @@ let cookies = {
     img: './src/assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
+    shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }
 
@@ -11,12 +12,14 @@ let twoCookies = [{
     img: './src/assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
+    shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }, {
     id: 1,
     img: './src/assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
+    shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }]
 
@@ -25,48 +28,56 @@ let lotsOfCookies = [{
     img: './src/assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
+    shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }, {
     id: 1,
     img: './src/assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
+    shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }, {
     id: 2,
     img: './src/assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
+    shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }, {
     id: 3,
     img: './src/assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
+    shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }, {
     id: 4,
     img: './src/assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
+    shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }, {
     id: 5,
     img: './src/assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
+    shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }, {
     id: 6,
     img: './src/assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
+    shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }, {
     id: 7,
     img: './src/assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
+    shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }]
 
