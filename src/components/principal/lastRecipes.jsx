8b6d1a94a2f15b77c6111577cf682ff27410c665
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-09-15 16:29:10 -0300

    feat: slider event handlers

--- src/components/principal/lastRecipes.jsx
+++ src/components/principal/lastRecipes.jsx
@@ -11,6 +11,20 @@ export function LastRecipes({ values }) {
     let prevRecipe = index > 0;
     let nextRecipe = index < values.length - 1;
 
+    function handlePrev() {
+        if (prevRecipe) {
+            setIndex(index - 1);
+        }
+        console.log(index);
+    }
+
+    function handleNext() {
+        if (nextRecipe) {
+            setIndex(index + 1)
+        }
+        console.log(index);
+    }
+
     let recipeImage = values[index];
 
     return(
