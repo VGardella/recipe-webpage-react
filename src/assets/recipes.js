39cf78d7e19e22c81197c0d076bc3056cf3bc63b
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-09-18 17:57:56 -0300

    add: mock recipes

--- src/assets/recipes.js
+++ src/assets/recipes.js
@@ -81,4 +81,35 @@ let lotsOfCookies = [{
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }]
 
+let food = [{
+    id: 0,
+    img: './src/assets/cookies.jpg',
+    title: 'Cookies',
+    category: 'Dessert',
+    shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
+    description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
+}, {
+    id: 1,
+    img: './src/assets/all-recipes-image.jpg',
+    title: 'Skewers',
+    category: 'Entree, Dinner, Lunch',
+    shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
+    description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
+}, {
+    id: 2,
+    img: './src/assets/bakery-image.jpg',
+    title: 'Bread',
+    category: 'Bakery',
+    shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
+    description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
+}, {
+    id: 3,
+    img: './src/assets/pancake-image.avif',
+    title: 'Pancakes',
+    category: 'Breakfast, Dessert',
+    shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
+    description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
+}]
+
 export { cookies, twoCookies, lotsOfCookies }
+export { food }
