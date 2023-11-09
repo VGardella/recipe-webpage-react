e359a5aaadb4b5be548597617a35da83025960f1
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-11-09 14:56:15 -0300

    Change images from 'src' to 'public' folder

--- public/assets/recipes.js
+++ public/assets/recipes.js
@@ -1,6 +1,6 @@
 let cookies = {
     id: 0,
-    img: './src/assets/cookies.jpg',
+    img: './public/assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
     shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
@@ -9,14 +9,14 @@ let cookies = {
 
 let twoCookies = [{
     id: 0,
-    img: './src/assets/cookies.jpg',
+    img: './public/assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
     shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }, {
     id: 1,
-    img: './src/assets/cookies.jpg',
+    img: './public/assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
     shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
@@ -25,56 +25,56 @@ let twoCookies = [{
 
 let lotsOfCookies = [{
     id: 0,
-    img: './src/assets/cookies.jpg',
+    img: './public/assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
     shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }, {
     id: 1,
-    img: './src/assets/cookies.jpg',
+    img: './public/assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
     shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }, {
     id: 2,
-    img: './src/assets/cookies.jpg',
+    img: './public/assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
     shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }, {
     id: 3,
-    img: './src/assets/cookies.jpg',
+    img: './public/assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
     shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }, {
     id: 4,
-    img: './src/assets/cookies.jpg',
+    img: './public/assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
     shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }, {
     id: 5,
-    img: './src/assets/cookies.jpg',
+    img: './public/assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
     shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }, {
     id: 6,
-    img: './src/assets/cookies.jpg',
+    img: './public/assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
     shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }, {
     id: 7,
-    img: './src/assets/cookies.jpg',
+    img: './public/assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
     shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
@@ -83,28 +83,28 @@ let lotsOfCookies = [{
 
 let food = [{
     id: 0,
-    img: './src/assets/cookies.jpg',
+    img: './public/assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
     shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }, {
     id: 1,
-    img: './src/assets/all-recipes-image.jpg',
+    img: './public/assets/all-recipes-image.jpg',
     title: 'Skewers',
     category: 'Entree, Dinner, Lunch',
     shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }, {
     id: 2,
-    img: './src/assets/bakery-image.jpg',
+    img: './public/assets/bakery-image.jpg',
     title: 'Bread',
     category: 'Bakery',
     shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }, {
     id: 3,
-    img: './src/assets/pancake-image.avif',
+    img: './public/assets/pancake-image.avif',
     title: 'Pancakes',
     category: 'Breakfast, Dessert',
     shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -6,21 +6,21 @@ export function Footer() {
             </div>
             <div id='media-links'>
                 <a className='external' href='https://github.com/VGardella' alt='Link to GitHub Profile'>
-                    <img className='media-icon' id='github' src='../src/assets/github.png'
-                        onMouseOver={e => (e.currentTarget.src='../src/assets/github-hover.png')}
-                        onMouseOut={e => (e.currentTarget.src='../src/assets/github.png')}
+                    <img className='media-icon' id='github' src='../public/assets/github.png'
+                        onMouseOver={e => (e.currentTarget.src='../public/assets/github-hover.png')}
+                        onMouseOut={e => (e.currentTarget.src='../public/assets/github.png')}
                     ></img>
                 </a>
                 <a className='external' href='https://www.linkedin.com/in/victoria-macarena-gardella-ruiz-a8bab2234/' alt='Link to LinkedIn Profile'>
-                    <img className='media-icon' id='linkedin' src='../src/assets/linkedin.png' alt=''
-                        onMouseOver={e => (e.currentTarget.src='../src/assets/linkedin-hover.png')}
-                        onMouseOut={e => (e.currentTarget.src='../src/assets/linkedin.png')}
+                    <img className='media-icon' id='linkedin' src='../public/assets/linkedin.png' alt=''
+                        onMouseOver={e => (e.currentTarget.src='../public/assets/linkedin-hover.png')}
+                        onMouseOut={e => (e.currentTarget.src='../public/assets/linkedin.png')}
                     ></img>
                 </a>
                 <a className='external' href='' alt='Send Mail'>
-                    <img className='media-icon' id='mail' src='../src/assets/mail.png' alt=''
-                        onMouseOver={e => (e.currentTarget.src='../src/assets/mail-hover.png')}
-                        onMouseOut={e => (e.currentTarget.src='../src/assets/mail.png')}
+                    <img className='media-icon' id='mail' src='../public/assets/mail.png' alt=''
+                        onMouseOver={e => (e.currentTarget.src='../public/assets/mail-hover.png')}
+                        onMouseOut={e => (e.currentTarget.src='../public/assets/mail.png')}
                     ></img>
                 </a>
             </div>
