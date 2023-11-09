47b33df88166f86e7740474dec4d68a56beebe57
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-11-09 15:15:58 -0300

    Updates

--- assets/recipes.js
+++ assets/recipes.js
@@ -1,6 +1,6 @@
 let cookies = {
     id: 0,
-    img: './public/assets/cookies.jpg',
+    img: 'assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
     shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
@@ -9,14 +9,14 @@ let cookies = {
 
 let twoCookies = [{
     id: 0,
-    img: './public/assets/cookies.jpg',
+    img: 'assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
     shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }, {
     id: 1,
-    img: './public/assets/cookies.jpg',
+    img: 'assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
     shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
@@ -25,56 +25,56 @@ let twoCookies = [{
 
 let lotsOfCookies = [{
     id: 0,
-    img: './public/assets/cookies.jpg',
+    img: 'assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
     shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }, {
     id: 1,
-    img: './public/assets/cookies.jpg',
+    img: 'assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
     shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }, {
     id: 2,
-    img: './public/assets/cookies.jpg',
+    img: 'assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
     shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }, {
     id: 3,
-    img: './public/assets/cookies.jpg',
+    img: 'assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
     shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }, {
     id: 4,
-    img: './public/assets/cookies.jpg',
+    img: 'assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
     shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }, {
     id: 5,
-    img: './public/assets/cookies.jpg',
+    img: 'assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
     shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }, {
     id: 6,
-    img: './public/assets/cookies.jpg',
+    img: 'assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
     shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }, {
     id: 7,
-    img: './public/assets/cookies.jpg',
+    img: 'assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
     shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
@@ -83,28 +83,28 @@ let lotsOfCookies = [{
 
 let food = [{
     id: 0,
-    img: './public/assets/cookies.jpg',
+    img: 'assets/cookies.jpg',
     title: 'Cookies',
     category: 'Dessert',
     shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }, {
     id: 1,
-    img: './public/assets/all-recipes-image.jpg',
+    img: 'assets/all-recipes-image.jpg',
     title: 'Skewers',
     category: 'Entree, Dinner, Lunch',
     shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }, {
     id: 2,
-    img: './public/assets/bakery-image.jpg',
+    img: 'assets/bakery-image.jpg',
     title: 'Bread',
     category: 'Bakery',
     shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
     description: 'Minim aliquip culpa deserunt consectetur tempor reprehenderit. Velit consequat veniam in velit consectetur.'
 }, {
     id: 3,
-    img: './public/assets/pancake-image.avif',
+    img: 'assets/pancake-image.avif',
     title: 'Pancakes',
     category: 'Breakfast, Dessert',
     shortDesc: 'Amet veniam officia sit incididunt sunt quis consectetur sunt aute voluptate esse proident nisi.',
