aba52719ded5e0802c4ce2f0847111d102e89412
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-11-09 15:30:55 -0300

    change images path

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -2,16 +2,16 @@ export function Header() {
     return(
         <div className='header'>
             <div className='header-section' id='header-top'>
-                <img className='user-info' id='logo' src='../public/assets/logo.png' alt='My notebook logo!'></img>
+                <img className='user-info' id='logo' src='assets/logo.png' alt='My notebook logo!'></img>
                 <div id='user-links'>
                     <a href=''>
-                        <img className='user-info' id='notifications' src='../public/assets/notification.png' alt='Notification icons created by Aldo Cervantes - Flaticon'
-                        onMouseOver={e => (e.currentTarget.src = '../public/assets/notification-hover.png')}
-                        onMouseOut={e => (e.currentTarget.src = '../public/assets/notification.png')}/>
+                        <img className='user-info' id='notifications' src='assets/notification.png' alt='Notification icons created by Aldo Cervantes - Flaticon'
+                        onMouseOver={e => (e.currentTarget.src = 'assets/notification-hover.png')}
+                        onMouseOut={e => (e.currentTarget.src = 'assets/notification.png')}/>
                     </a>
                     <div className="user">
                         <a href=''>
-                            <img className='user-info' id='user-icon' src='../public/assets/profile-icon.png' alt='' />
+                            <img className='user-info' id='user-icon' src='assets/profile-icon.png' alt='' />
                         </a>
                         <div id="user-greeting">
                             <p className='user-info' id='user-hi'>Buenos días,</p>
@@ -23,20 +23,20 @@ export function Header() {
             <div className='header-section' id='header-bottom'>
                 <div id='search'>
                     <button type='submit' id='search-button'>
-                        <img id='search-img' src='../public/assets/magnifying_glass.png' alt='Haceme click para buscar una receta!' />
+                        <img id='search-img' src='assets/magnifying_glass.png' alt='Haceme click para buscar una receta!' />
                     </button>
                     <input type='search' id='searchbar' />
                 </div>
                 <div id='page-links'>
                     <a href='' alt='' id=''>
-                        <img className='link-img' src='../public/assets/principal.png'></img>
+                        <img className='link-img' src='assets/principal.png'></img>
                         Principal
                     </a>
                     <a href='' alt='' id=''>
-                        <img className='link-img' src='../public/assets/all-recipes-cookie.png'></img>
+                        <img className='link-img' src='assets/all-recipes-cookie.png'></img>
                         Todas las recetas</a>
                     <a href='' alt='' id=''>
-                        <img className='link-img' src='../public/assets/add-recipe.png'></img>
+                        <img className='link-img' src='assets/add-recipe.png'></img>
                         Añadir receta</a>
                 </div>
             </div>
